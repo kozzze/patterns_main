272f3add02c0b1c6fe06e686ff821baa95e66174
kozzze <kozlov.ed.vs@mail.ru>
2024-09-25 00:23:59 +0300

    LAB 1 Task 6
    Дописал код который принимает как аргумент два
    значения. Первое значение говорит, какой из методов задачи 1
    выполнить, второй говорит о том, откуда читать список аргументом
    должен быть написан адрес файла. Далее необходимо прочитать массив
    и выполнить метод.

--- acquaintance_with_ruby/work_with_arrays.rb
+++ acquaintance_with_ruby/work_with_arrays.rb
@@ -21,6 +21,43 @@ def positiv_numb(array)
   return -1  # Если положительных элементов нет
 end
 
-array = [1,-4,2,-5,4,6,8,9]
-puts "Минимальный элемент массива = #{min_array(array)}"
-puts "Первый положительный элемент находся под индексом - #{positiv_numb(array)}"
+# Функция для чтения массива из файла
+def read(path)
+  begin
+    file = File.open(path, "r")
+    ar = file.read.split.map(&:to_i)
+    file.close
+    ar
+  rescue => e
+    puts "Ошибка при чтении файла: #{e.message}"
+    []
+  end
+end
+
+if ARGV.size == 2
+  method = ARGV[0].to_i
+  file = ARGV[1]
+  array = read(file)
+
+  if array.empty?
+    puts "Массив пустой, проверьте файл."
+    exit
+  end
+
+  puts "Массив: [#{array.join(', ')}]"
+
+  if method == 1
+    puts "Минимальное значение в массиве: #{min_array(array)}"
+  elsif method == 2
+    ind = positiv_numb(array)
+    if ind == -1
+      puts "Положительных элементов в массиве нет."
+    else
+      puts "Первое положительное значение находится под индексом #{ind} - (для программистов); #{ind + 1} - (для обычных людей)"
+    end
+  else
+    puts "Неизвестный метод. Пожалуйста, выберите 1 или 2"
+  end
+else
+  puts "Введите два аргумента: метод и путь к файлу."
+end
