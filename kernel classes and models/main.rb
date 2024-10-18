ddec69232fad75188e42a720fc23956cc22bbc17
kozzze <kozlov.ed.vs@mail.ru>
2024-10-18 19:06:39 +0300

    lab2 delete validate on kostruktor. fix setter

--- kernel classes and models/main.rb
+++ kernel classes and models/main.rb
@@ -1,27 +1,9 @@
 require_relative 'student'
-begin
-  student1=Student.new(id:1, lastname: 'Голова',firstname: 'Максим', surname: 'Иванович',phone: '+71936486347',telegram: '@golova',email: 'golova@example.com',github: 'https://github.com/golova')
-rescue ArgumentError => e
-  puts e.message
-end
-begin
-  student2 = Student.new(id:2, lastname: 'Нога', surname: 'Максимович',firstname: 'Иван',phone: '89174657428',telegram: '@noga',email: 'noga@example.com',github: 'https://github.com/noga')
-rescue ArgumentError => e
-  puts e.message
-end
 
-begin
-  student3 = Student.new(id:3, lastname: 'Рука',firstname: 'Александр', surname: 'Савельевич',telegram: '@tyrew',github: 'https://github.com/tyrew')
-rescue ArgumentError => e
-  puts e.message
-  end
-begin
-  student4 = Student.new(id: 4, lastname: "Петров", firstname: "Петр", surname: "Петрович", phone: "+89476326547",github: 'https://github.com/gghfdfa')
-rescue ArgumentError => e
-  puts e.message # Ожидаем сообщение о некорректном номере телефона
-end
+student1=Student.new(id:1, lastname: 'Голова',firstname: 'Максим', surname: 'Иванович',telegram: '@golova',email: 'golova@example.com',github:'https://github.com/noga')
+student2 = Student.new(id:2, lastname: 'Нога', surname: 'Максимович',firstname: 'Иван',phone: '89174657428',telegram: '@noga',email: 'noga@example.com',github: 'https://github.com/noga')
+puts student1.validate
+puts student2.validate
 
-student1.display_info
-student2.display_info
-student3.display_info
-student4.display_info
+puts student1.display_info
+puts student2.display_info
--- kernel classes and models/student.rb
+++ kernel classes and models/student.rb
@@ -1,27 +1,51 @@
 #Создаем класс Student
 class Student
-  #Геттеры обязательные поля
-  attr_reader :id, :lastname, :firstname, :surname
-  #Геттеры для контактов
-  attr_reader :phone, :telegram, :email, :github
 
-  def initialize(id:, lastname:, firstname:, surname:, phone:nil, telegram:nil, email:nil, github:nil)
+  attr_accessor :lastname, :firstname, :surname, :github
+ 
+  attr_reader :id,:phone, :telegram, :email
+
+  def initialize(id:nil, lastname:, firstname:, surname:, phone:nil, telegram:nil, email:nil, github:nil)
     @id=id
-    @lastname=lastname
-    @firstname=firstname
-    @surname=surname
-    raise ArgumentError, "Фамилия введена неверно:  #{@lastname}" unless self.class.name_valid?(@lastname)
-    raise ArgumentError, "Имя введена неверно:  #{@firstname}" unless self.class.name_valid?(@firstname)
-    raise ArgumentError, "Отчетво введена неверно:  #{@surname}" unless self.class.name_valid?(@surname)
-    @github = github
-    if github && !self.class.valid_github?(github)
-      raise ArgumentError, "GitHub введен неверно: #{github}"
-    end
+    self.lastname=lastname
+    self.firstname=firstname
+    self.surname=surname
+    self.github = github if github
     set_contacts(phone: phone, telegram: telegram, email: email)
-    validate
   end
 
+  def github=(github)
+    if self.class.valid_github?(github)
+      @github = github
+    else
+      raise ArgumentError, "GitHub введен неверно: #{github}"
+    end
+  end 
 
+  def lastname=(lastname)
+    if self.class.name_valid?(lastname)
+      @lastname = lastname
+    else
+      raise ArgumentError, "Фамилия введена неверно: #{lastname}"
+    end
+  end
+
+  def firstname=(firstname)
+    if self.class.name_valid?(firstname)
+      @firstname = firstname
+    else
+      raise ArgumentError, "Имя введено неверно: #{firstname}"
+    end
+  end
+
+  def surname=(surname)
+    if self.class.name_valid?(surname)
+      @surname = surname
+    else
+      raise ArgumentError, "Отчество введено неверно: #{@surname}"
+    end
+  end
+  
   def to_s
     "ID: #{@id}, ФИО: #{@lastname} #{@firstname} #{@surname}, Номер телефона: #{@phone}, Телеграм: #{@telegram}, Почта: #{@email}, GitHub: #{@github}"
   end
@@ -39,17 +63,17 @@ class Student
 
   # Валидация GitHub
   def self.valid_github?(github)
-    github && github=~/^(https?:\/\/)?(www\.)?github\.com\/[A-Za-z0-9._-]+$/
+     github=~/^(https?:\/\/)?(www\.)?github\.com\/[A-Za-z0-9._-]+$/
   end
 
   # Валидация почты
   def self.mail_valid?(mail)
-    mail && mail =~ /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+     mail =~ /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
   end
 
   # Валидация телеграмма
   def self.tg_valid?(tg)
-    tg && tg =~ /^@[A-Za-z0-9_]{1,32}$/
+    tg =~ /^@[A-Za-z0-9_]{1,32}$/
   end
 
   # Метод для проверки существования контакта и гита
@@ -72,34 +96,34 @@ class Student
 
   # Метод для установки значений полей контактов
   def set_contacts(phone: nil, telegram: nil, email: nil)
-    if phone && !self.class.phone_valid?(phone)
+    if phone &&!self.class.phone_valid?(phone)
       raise ArgumentError, "Номер введен неверно: #{phone}"
+    else
+      @phone = phone
     end
-    if telegram && !self.class.tg_valid?(telegram)
+    if telegram &&!self.class.tg_valid?(telegram)
       raise ArgumentError, "Телеграм введен неверно: #{telegram}"
+    else
+      @telegram = telegram
     end
     if email && !self.class.mail_valid?(email)
       raise ArgumentError, "Email введен неверно: #{email}"
+    else
+      @email = email
     end
-
-    # Устанавливаем значения контактов
-    @phone = phone if phone
-    @telegram = telegram if telegram
-    @email = email if email
   end
 
   # Метод для вывода на экран сведений о студенте
   def display_info
-    puts "Сведения о студенте:"
-    puts "ID: #{@id}"
-    puts "Фамилия: #{@lastname}"
-    puts "Имя: #{@firstname}"
-    puts "Отчество : #{@surname}"
-    puts "Телефон : #{@phone}" if @phone
-    puts "Телеграм: #{@telegram}" if @telegram
-    puts "Почта: #{@email}" if @email
-    puts "GitHub: #{@github}" if @github
-    puts "\n"
-
+    str=[]
+    str.push( "ID: #{@id}")
+    str.push( "Фамилия: #{@lastname}")
+    str.push( "Имя: #{@firstname}")
+    str.push( "Отчество : #{@surname}")
+    str.push( "Телефон : #{@phone}") if @phone
+    str.push( "Телеграм: #{@telegram}") if @telegram
+    str.push( "Почта: #{@email}") if @email
+    str.push( "GitHub: #{@github}") if @github
+    str.join(" ")
   end
 end
