dcaef5e5a7cae465b86930882ece64ea7f3b68e6
kozzze <kozlov.ed.vs@mail.ru>
2024-11-15 18:16:33 +0300

    lab3 t.1.1.18 add find_defore_min method
    Дан целочисленный массив. Необходимо найти элементы, расположенные перед первым минимальным

--- polymorphism_blocks/blocks_methods.rb
+++ polymorphism_blocks/blocks_methods.rb
@@ -1,3 +1,10 @@
+# Циклический сдвиг элементов массива влево на три позиции.
 def shift_left(arr)
   arr.rotate(3)
 end
+
+#Hайти элементы, расположенные перед первым минимальным
+def find_before_min(arr)
+  arr[0...arr.index(arr.min)]
+end
+
