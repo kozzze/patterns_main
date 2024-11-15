ed2d64f71c28a44ac59a55091853b4f73ac0be27
kozzze <kozlov.ed.vs@mail.ru>
2024-11-15 18:34:38 +0300

    lab 3 1.1.30 add local_max? method returning true or false
    Дан целочисленный массив и натуральный индекс (число, меньшее размера массива). Необходимо определить является ли элемент по указанному индексу локальным максимумом.

--- polymorphism_blocks/blocks_methods.rb
+++ polymorphism_blocks/blocks_methods.rb
@@ -7,4 +7,14 @@ end
 def find_before_min(arr)
   arr[0...arr.index(arr.min)]
 end
+#Необходимо определить является ли элемент по указанному индексу локальным максимумом. (Число меньше длинны массива)
+def local_max?(arr,ind)
+   if ind > 0 && ind < arr.length && arr[ind] > arr[ind-1] && arr[ind] > arr[ind+1]
+    true
+  end
+  false
+end 
 
+arr=[1,3,3,3,4,1,5]
+n=3
+puts local_max?(arr,n)
