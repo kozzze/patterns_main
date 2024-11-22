672bc6ed17ab211f1332d5560ab70509d54259a2
kozzze <kozlov.ed.vs@mail.ru>
2024-11-22 20:22:46 +0300

    lab 3 t2.6 create ArrayProcessing class && add detect method

---
+++ polymorphism_blocks/processing_array.rb
@@ -0,0 +1,29 @@
+#6,18,30,31,19,7
+
+class ArrayProcessing
+
+  attr_reader :array
+
+  def initialize(array)
+    self.array = array
+  end
+
+  def array=(array)
+    if array.is_a? (Array)
+      @array = array
+    else
+      raise TypeError.new('Входные данные неверны')
+    
+    end
+  end
+
+  # Метод detect
+  def detect
+    array.each do |element|
+      return element if yield(element)
+    end
+    nil
+  end
+
+
+end
