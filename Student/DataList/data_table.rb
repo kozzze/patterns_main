cbb2c9176347f4e3debe2bf91cdd4d8909b7931c
kozzze <kozlov.ed.vs@mail.ru>
2024-12-13 20:07:45 +0300

    lab4 t1.1 add get_element row_count col_count methods

--- Student/DataList/data_table.rb
+++ Student/DataList/data_table.rb
@@ -1,14 +1,44 @@
 class DataTable
   def initialize(data)
-    unless data.is_a?(Array) && data.all? { |row| row.is_a?(Array) }
-      raise ArgumentError, "Data must be a two-dimensional array"
-    end
-    @data = data
+    self.data = data
+  end
+
+  def get_element(num_of_row, num_of_column)
+    @data[num_of_row][num_of_column]
+  end
+
+  def row_count
+    @data.size
+  end
+
+  def col_count
+    @data.empty? ? 0 : @data[0].size
+  end
+
+  def to_s
+    @data.inspect
   end
 
   private
 
-  def data
-    @data
+  attr_reader :data
+
+  def data=(data)
+    validate_data(data)
+    @data = deep_dup(data)
+  end
+
+  def validate_data(data)
+    unless data.is_a?(Array) && data.all? { |row| row.is_a?(Array) }
+      raise ArgumentError, "Данные должны быть в виде двумерного массива"
+    end
+  end
+
+  def deep_dup(element)
+    if element.is_a?(Array)
+      element.map { |sub_element| deep_dup(sub_element) }
+    else
+      element.dup rescue element
+    end
   end
 end
